c52139749bac20f8ffafc6ff9834702954e8e861
matebonfanti <matebonfanti@gmail.com>
2024-02-03 14:06:15 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -36,6 +37,11 @@ public class Program {
 		for(Seller e : list) {
 			System.out.println(e);
 		}
+		
+		System.out.println("==== TEST 3: seller Insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 	}
 
 }
