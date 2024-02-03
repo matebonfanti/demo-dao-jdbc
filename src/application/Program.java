c1a9a402c5bd1b2a7c312e5aac2810dfbe7df56d
matebonfanti <matebonfanti@gmail.com>
2024-02-03 13:36:12 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,6 +28,14 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		
+		System.out.println("==== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		
+		for(Seller e : list) {
+			System.out.println(e);
+		}
 	}
 
 }
