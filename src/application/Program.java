f2ad8e37234f6c8a54ca23d63e62cf3a8cb45327
matebonfanti <matebonfanti@gmail.com>
2024-02-03 14:41:31 -0300

    Implementing Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,10 +38,21 @@ public class Program {
 			System.out.println(e);
 		}
 		
-		System.out.println("==== TEST 3: seller Insert =====");
+		System.out.println("==== TEST 4: seller Insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("==== TEST 5: seller Update =====");
+		
+		seller = sellerDao.findById(1);
+		
+		seller.setName("Martha Waine");
+		
+		sellerDao.update(seller);
+		
+		System.out.println("Updated Completed");
+		
 	}
 
 }
